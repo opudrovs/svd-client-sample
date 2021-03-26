fdf21c8ea87e610297cd40fe9e41fd2d38aea3c2
Olga Pudrovska <opudrovs@gmail.com>
2021-03-26 11:30:33 +0200

    Remove unneeded comment.

--- pages/404.js
+++ pages/404.js
@@ -6,11 +6,6 @@ import ErrorPage from 'components/pages/root/ErrorPage';
 
 import { errorMessages } from 'constants/MessageConstants';
 
-/**
- * Next.js custom document component.
- * Renders React components added to it on all pages of the application
- * in addition to the specific content of the pages.
- */
 
 const Custom404 = () => <ErrorPage errorCode={404} errorMessage={errorMessages[404]} />;
 
