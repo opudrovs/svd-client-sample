7876e214d334df778b50c3a15aca04a3e187645d
Olga Pudrovska <opudrovs@gmail.com>
2021-03-30 18:30:12 +0300

    Enable support for Webpack 5.

--- next.config.js
+++ next.config.js
@@ -3,5 +3,8 @@ module.exports = {
     sassOptions: {
         // Scss code that you want to be prepended to every single scss file.
         prependData: '@import \'styles/theme\';'
+    },
+    future: {
+        webpack5: true
     }
 };
