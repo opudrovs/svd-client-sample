6618c5abd4926589407e1f060086bb892ec089fe
Olga Pudrovska <opudrovs@gmail.com>
2021-03-30 20:08:12 +0300

    Add syncing Redux store state across tabs.

--- src/redux/store.js
+++ src/redux/store.js
@@ -16,34 +16,44 @@ import {
     persistReducer
 } from 'redux-persist';
 
+import { createStateSyncMiddleware, initStateWithPrevTab } from 'redux-state-sync';
+
 /* REDUX */
 
 import checkoutReducer from './checkout/checkoutSlice';
 import storage from './storage';
 
+/* UTILS */
+
+import { isBrowser } from 'utils/SystemUtils';
+
 
 /**
- * Redux store with support for persisting Redux data.
+ * Redux store with support for persisting Redux store locally and syncing Redux store state across tabs.
  */
 
-const persistConfig = {
-    key: 'root',
-    storage,
-};
-
-const reducer = combineReducers({
+const rootReducer = combineReducers({
     checkout: checkoutReducer
 });
 
-const persistedReducer = persistReducer(persistConfig, reducer);
+const IGNORED_ACTIONS = [FLUSH, PAUSE, PERSIST, PURGE, REGISTER, REHYDRATE];
+
+const IS_BROWSER = isBrowser();
 
 const store = configureStore({
-    reducer: persistedReducer,
+    reducer: persistReducer({
+        key: 'root',
+        storage,
+    }, rootReducer),
     middleware: getDefaultMiddleware({
         serializableCheck: {
-            ignoredActions: [FLUSH, PAUSE, PERSIST, PURGE, REGISTER, REHYDRATE]
+            ignoredActions: IGNORED_ACTIONS
         }
-    })
+    }).concat(IS_BROWSER ? createStateSyncMiddleware({ blacklist: IGNORED_ACTIONS }) : [])
 });
+
+if (IS_BROWSER) {
+    initStateWithPrevTab(store);
+}
 
 export default store;
