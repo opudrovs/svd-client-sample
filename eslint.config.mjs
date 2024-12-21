56ac613142335a2f94b69125e005fab5dcbf2945
Olga Pudrovska <olga@weave.works>
2024-12-21 03:47:26 +0100

    Start reworking linting.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,14 @@ import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
 
+import js from '@eslint/js'; 
+import react from 'eslint-plugin-react'; 
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import next from '@next/eslint-plugin-next';
+import prettier from 'eslint-plugin-prettier';
+import prettierConfig from 'eslint-config-prettier';
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
@@ -9,6 +17,41 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [...compat.extends("next/core-web-vitals")];
+export default [
+  ...compat.extends('next/core-web-vitals'),
+  js.configs.recommended,
+  react.configs.recommended,
+  next.configs.recommended,
+  prettierConfig,
+  {
+    files: ['**/*.js', '**/*.jsx', '**/*.ts', '**/*.tsx'],
+    ignores: [
+      'node_modules/**',
+      '.next/**',
+      'public/**',
+      'dist/**',
+      'src/data',
+      'src/redux/storage.js',
+      '*.test.js',
+    ],
+    plugins: {
+      react,
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+      prettier,
+    },
+    rules: {
+      ...js.configs.recommended.rules,
+      ...react.configs.recommended.rules,
+      ...react.configs['jsx-runtime'].rules,
+      ...reactHooks.configs.recommended.rules,
+      'react/no-unescaped-entities': 'off',
+      'react/jsx-no-target-blank': 'off',
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+    },
+  },
+];
 
-export default eslintConfig;
