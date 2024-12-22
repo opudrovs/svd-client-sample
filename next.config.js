490a207a6be841ec219737d40c0de4496f1772d0
Olga Pudrovska <olga@weave.works>
2024-12-22 14:22:44 +0100

    Update SCSS code.

--- next.config.js
+++ next.config.js
@@ -1,9 +1,18 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+// Define __dirname in ES modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
   sassOptions: {
-      // Scss code that you want to be prepended to every single scss file.
-      prependData: '@import \'styles/theme\';'
+    // Prepend theme styles to every SCSS file
+    additionalData: `@use 'styles/theme' as *;`, // This makes theme.scss accessible
+    includePaths: [path.join(__dirname, 'styles')], // Add your styles folder to SCSS include paths
   },
 };
 
